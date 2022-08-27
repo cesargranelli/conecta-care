de935b23d6cbe2854da41ea35a3959ab1760e553
Cesar <cesar.granelli.dev@gmail.com>
2022-08-27 11:17:31 -0300

    Configuração para execução em multi module

--- persistence/target/generated-sources/annotations/com/sevenine/conecta/mappers/impl/DominioMapperImpl.java
+++ persistence/target/generated-sources/annotations/com/sevenine/conecta/mappers/impl/DominioMapperImpl.java
@@ -37,7 +37,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-08-25T22:03:15-0300",
+    date = "2022-08-27T11:16:43-0300",
     comments = "version: 1.5.2.Final, compiler: javac, environment: Java 14.0.1 (Oracle Corporation)"
 )
 public class DominioMapperImpl implements DominioMapper {
